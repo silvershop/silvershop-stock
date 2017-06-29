84d4726b42bdfa7b934a51c94ddc6421e744e0e0
Will Rossiter <will@fullscreen.io>
2017-06-30 08:18:41 +1200

    Check method exists (Fixes #11)

--- code/extensions/StockManagedOrderItem.php
+++ code/extensions/StockManagedOrderItem.php
@@ -5,16 +5,19 @@
  */
 class StockManagedOrderItem extends DataExtension
 {
-
     public function onPlacement()
     {
         if ($this->owner->ProductVariationID) {
             if ($variation = $this->owner->ProductVariation()) {
-                $variation->decrementStock($this->owner);
+                if ($variation->hasMethod('decrementStock')) {
+                    $variation->decrementStock($this->owner);
+                }
             }
         } elseif ($this->owner->ProductID) {
             if ($product = $this->owner->Product()) {
-                $product->decrementStock($this->owner);
+                if ($product->hasMethod('decrementStock')) {
+                    $product->decrementStock($this->owner);
+                }
             }
         }
     }
